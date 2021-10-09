a76cb67f152c449c6f55d01ff910e81301ac27dc
Drozdova-Daria <60818854+Drozdova-Daria@users.noreply.github.com>
2021-10-09 17:56:58 +0300

    Update lab1_1.R

--- lab1/lab1_1.R
+++ lab1/lab1_1.R
@@ -1,6 +1,6 @@
 library(stringr)
 
-fix_data <- function (elem) {
+update_data <- function (elem) {
   str_w_spaces <- str_replace_all(elem, " ", "")
   if (is.na(as.numeric(str_w_spaces))) {
     return(elem)
@@ -9,6 +9,9 @@ fix_data <- function (elem) {
   }
 }
 
-# Read file
+fix_data <- function (data) {
+  return(data.frame((apply(data, MARGIN=c(1,2), FUN = update_data))))
+}
+
 myfile = read.csv('test_data_01.csv')
-print(data.frame((apply(myfile, MARGIN=c(1,2), FUN = fix_data))))
+print(fix_data(myfile))
